79662d765c1a3080fe32f699e6d0149627d5f15a
alinchees <marnopolskaaalina@gmail.com>
2023-01-04 01:23:26 +0200

    m

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,65 @@
-import { Component } from "react"
-import Form from "./Form/Form";
-import Contacts from "./Contacts/Contacts";
+import { Component } from 'react';
+import Form from './Form/Form';
+import Contacts from './Contacts/Contacts';
 class App extends Component {
-    state = {
+  state = {
     contacts: [],
     filter: '',
-  }
-  delete=(deleted)=>{
-    const{contacts} = this.state;
-    this.setState({contacts : contacts.filter((cont)=> cont.id !== deleted)});
-}
-filtered=()=>{
-  if (this.state.filter) {
-      return this.state.contacts.filter(m => m.name.toLowerCase().includes(this.state.filter.toLocaleLowerCase()))
-  }
-  return this.state.contacts;
-}
-addTar=(even)=>{
-  const r = even.target.value;
-  this.setState({filter: r});
-}
-  nameContact =(data)=>{
-    const{name,number} = data;
-    const bulElement =  this.state.contacts.find(con=> con.name === name);
-    if (bulElement) {
-      alert(`${name} is already contact`);
+  };
+  delete = deleted => {
+    const { contacts } = this.state;
+    this.setState({ contacts: contacts.filter(cont => cont.id !== deleted) });
+  };
+  filtered = () => {
+    if (this.state.filter) {
+      return this.state.contacts.filter(m =>
+        m.name.toLowerCase().includes(this.state.filter.toLocaleLowerCase())
+      );
     }
-    else{
-      this.setState((prev) => ({
-              contacts : [...prev.contacts,{id: this.state.contacts.length,name, number}]
-            }))
-    }
-  }
+    return this.state.contacts;
+  };
+  addTar = even => {
+    const r = even.target.value;
+    this.setState({ filter: r });
+  };
 
-  render(){
+  
+  nameContact = ({ name, number, id }) => {
+    this.state.contacts.find(con => con.name === name)
+      ? alert(`${name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [{ name, number, id }, ...prevState.contacts],
+        }));
+  };
+
+  
+  render() {
     const filteredContacts = this.filtered();
     return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-        color: '#010101'
-      }}
-    >
-      <h1>Phonebook</h1>
-      <Form submit={this.nameContact}/>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <Form submit={this.nameContact} />
 
-  <h2>Contacts</h2>
-  {this.state.contacts.length?
-      <Contacts cont={filteredContacts} delete={this.delete} add={this.addTar}/>
-      :""}
-    </div>
-    
-  );
+        <h2>Contacts</h2>
+        {this.state.contacts.length ? (
+          <Contacts
+            cont={filteredContacts}
+            delete={this.delete}
+            add={this.addTar}
+          />
+        ) : (
+          ''
+        )}
+      </div>
+    );
   }
-  
-};
+}
 export default App;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,30 +1,42 @@
-import { Component } from "react";
-import  PropTypes  from "prop-types";
-class Contacts extends Component{ 
-render(){
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+class Contacts extends Component {
+  render() {
     console.log(this.props.cont);
-    return(
-        <div>
-            <ul>
-                {this.props.cont.map(m=>(
-                    <li key={m.id}>
-                        <p>{m.name}: {m.number}</p>
-                        <button type="button" onClick={()=>this.props.delete(m.id)} >delete</button>
-                    </li>
-                ))}
-            </ul>
-            <h2>Find firs name</h2>
-           <input type="text" 
-            name="firstName"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            onChange={this.props.add}/>
-        </div>
-    )
-}
+    return (
+      <div>
+        <ul>
+          {this.props.cont.map(m => (
+            <li key={m.id}>
+              <p>
+                {m.name}: {m.number}
+              </p>
+              <button type="button" onClick={() => this.props.delete(m.id)}>
+                delete
+              </button>
+            </li>
+          ))}
+        </ul>
+        <h2>Find firs name</h2>
+        <input
+          type="text"
+          name="firstName"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          onChange={this.props.add}
+        />
+      </div>
+    );
+  }
 }
 Contacts.propTypes = {
-  cont: PropTypes.array,
-  delete: PropTypes.func,
-  add: PropTypes.func
-}
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
+  
 export default Contacts;
